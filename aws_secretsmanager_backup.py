380a78d0c67175d104bdafa6d75aaf70bc349a41
Dan Farmer <19284182+reedbug@users.noreply.github.com>
2019-09-07 14:47:40 +0100

    Paginate through secrets

--- aws_secretsmanager_backup.py
+++ aws_secretsmanager_backup.py
@@ -7,6 +7,7 @@
 
 import argparse
 import logging
+import boto3
 
 def main():
     """Dump AWS Secrets Manager Secret names and values to stdout."""
@@ -14,6 +15,22 @@ def main():
     args = parse_args()
     if args.loglevel:
         logging.basicConfig(level=args.loglevel)
+
+    secrets_client = boto3.client('secretsmanager')
+
+    for secret in get_secrets(secrets_client):
+        print(secret['Name'])
+
+def get_secrets(secrets_client):
+    """Iterator for secrets.
+
+    Yield secrets
+    """
+    secrets_paginator = secrets_client.get_paginator('list_secrets')
+    pages = secrets_paginator.paginate()
+    for page in pages:
+        for secret in page['SecretList']:
+            yield secret
 
 def parse_args():
     """Create arguments and populate variables from args.
